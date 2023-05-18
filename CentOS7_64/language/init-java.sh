2de396f34dde720a488d92152847e7784cc577e4
HuangSouth <2168626265@qq.com>
2023-05-18 17:39:03 +0800

    添加nacos安装脚本

--- CentOS7_64/language/init-java.sh
+++ CentOS7_64/language/init-java.sh
@@ -8,16 +8,14 @@
 install_dir="/usr/local/java"
 
 # 创建安装目录
-sudo mkdir -p $install_dir
+sudo mkdir -p "$install_dir"
 
 # 下载OpenJDK 8安装包
 sudo yum install -y java-1.8.0-openjdk-devel
 
 # 设置环境变量
-sudo tee /etc/profile.d/java.sh <<EOF
-export JAVA_HOME=$install_dir
-export PATH=\$PATH:\$JAVA_HOME/bin
-EOF
+echo "export JAVA_HOME=$install_dir" | sudo tee /etc/profile.d/java.sh
+echo 'export PATH="$JAVA_HOME/bin:$PATH"' | sudo tee -a /etc/profile.d/java.sh
 
 # 刷新环境变量
 source /etc/profile.d/java.sh
@@ -25,8 +23,4 @@ source /etc/profile.d/java.sh
 # 验证安装结果
 java -version
 
-echo " "
-echo " "
-echo " "
-
-echo "安装版本: 1.8.0   安装目录: /usr/local/java"
+echo "安装版本: 1.8.0   安装目录: $install_dir"
---
+++ CentOS7_64/server/nacos/install-nacos.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+
+# version:      1.0
+# state:        finish
+# updateTime:   2023年5月18日17:38:43
+
+
+mkdir /opt/nacos
+
+cd /opt/nacos
+
+### [下载nacos-2.1.0]
+wget https://github.com/alibaba/nacos/releases/download/2.1.0/nacos-server-2.1.0.tar.gz
+
+tar -zxvf nacos-server-2.1.0.tar.gz
+
+### [输入 MySQL 用户名和密码]
+read -p "MySQL Username: " username
+read -s -p "MySQL Password: " password
+echo
+
+### 进入 MySQL 并创建数据库
+mysql -u "$username" -p"$password" <<EOF
+CREATE DATABASE nacos;
+use nacos;
+source /opt/nacos/conf/nacos-mysql.sql;
+EOF
+
+### [解除防火墙限制]
+firewall-cmd --permanent --add-port=8848/tcp
+
+### [启动]
+echo 'If you need to start the need to install jdk8 and version, and enter the /opt/nacos/nacos/bin/startup.sh -m standalone command'
+# /opt/nacos/nacos/bin/startup.sh -m standalone
+
+
+## http://自己IP:8848/nacos/index.html
+
